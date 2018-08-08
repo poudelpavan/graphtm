9aaffd0449dad8f5b7addcffd8ca7cfa90edbfdb
poudelpavan <pvnpdl1@gmail.com>
2018-08-08 13:55:12 -0400

    Grid graph and nodes created.

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
     private static ArrayList<ArrayList<Transaction>> nodal_txs = new ArrayList<ArrayList<Transaction>>(total_nodes);
 
     /*
-     * Generate a grid graph
+     * Generate a grid graph.
      */
     public static Graphs generateGridGraph(int gridsize){
         Graphs grid = new Graphs();
@@ -36,7 +36,7 @@ public class Main {
     }
 
     /*
-     * Get neighbors of a node in grid graph
+     * Get neighbors of a node in grid graph.
      */
     public static ArrayList<Integer> getNeighbors(int r, int c, int rows, int cols){
         ArrayList<Integer> neighbors = new ArrayList<Integer>();
@@ -56,7 +56,7 @@ public class Main {
     }
 
     /*
-     * Generate nodes for grid graph
+     * Generate nodes for grid graph.
      */
     public static ArrayList<Node> generateNodes(int gridsize){
         ArrayList<Node> nodes = new ArrayList<Node>();
@@ -76,14 +76,14 @@ public class Main {
     }
 
     /*
-     * Calculate distance (communication cost) between two nodes in grid graph
+     * Calculate distance (communication cost) between two nodes in grid graph.
      */
     public static int getCommCost(Node a, Node b){
         return (abs(a.getX() - b.getX()) + abs(a.getY() - b.getY()));
     }
 
     /*
-     * Read-Write set for a transaction is randomly calculated
+     * Generate Read-Write set for a transaction with random size.
      */
     public static ArrayList<Transaction> generateTransactions(int tot_obj, int tot_tx, int updt_rate){
         ArrayList<Transaction> txs = new ArrayList<Transaction>(tot_tx);
@@ -121,7 +121,7 @@ public class Main {
     }
 
     /*
-     * Read-Write set for a transaction is fixed
+     * Generate Read-Write set for a transaction with fixed size.
      */
     public static ArrayList<Transaction> generateTransactions(int tot_obj, int tot_tx, int updt_rate, int rws_size){
         ArrayList<Transaction> txs = new ArrayList<Transaction>(tot_tx);
@@ -156,6 +156,9 @@ public class Main {
         return txs;
     }
 
+    /*
+     * Generate a list of unique random numbers.
+     */
     public static ArrayList<Integer> getRandList(int size, int min, int max){
         Random rand = new Random();
         ArrayList<Integer> randList = new ArrayList<Integer>();
@@ -169,6 +172,26 @@ public class Main {
         return randList;
     }
 
+    /*
+     * Generate a random Read-Write set.
+     */
+    private static List<Objects> setRWSet(int rw_size, int total_objs){
+        List<Objects> rw = new ArrayList<Objects>();
+        int sum = 0;
+        Random rand = new Random();
+        ArrayList<Integer> randList = new ArrayList<Integer>();
+
+        while (sum<rw_size) {
+            int x = rand.nextInt(total_objs);
+            if (!randList.contains(x)) {
+                randList.add(x);
+                randList.add(x);
+                rw.add(objs.get(x));
+                sum = sum + objs.get(x).getObj_size();
+            }
+        }
+        return rw;
+    }
 
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
@@ -278,71 +301,4 @@ public class Main {
             System.out.print(")\n");
         }
     }
-
-    private static List<Objects> setRWSet(int rw_size, int total_objs){
-        List<Objects> rw = new ArrayList<Objects>();
-        int sum = 0;
-        Random rand = new Random();
-        ArrayList<Integer> randList = new ArrayList<Integer>();
-
-        while (sum<rw_size) {
-            int x = rand.nextInt(total_objs);
-            if (!randList.contains(x)) {
-                randList.add(x);
-                randList.add(x);
-                rw.add(objs.get(x));
-                sum = sum + objs.get(x).getObj_size();
-            }
-        }
-        return rw;
-    }
-
-    /*private static List<Objects> getRWSet(int update_rate, int rw_set_size, List<Objects> rwset, int rw){
-        List<Objects> ws = new ArrayList<Objects>();
-        List<Objects> rs = new ArrayList<Objects>();
-        int ws_size = rw_set_size * update_rate/800;
-        int rs_size = (rw_set_size/8) - ws_size;
-        int n = 0, sum =0;
-        Random rand = new Random();
-        ArrayList<Integer> randList = new ArrayList<Integer>();
-        while (randList.size() < rwset.size()) {
-            int a = rand.nextInt(rwset.size());
-            if (!randList.contains(a)) {
-                randList.add(a);
-            }
-        }
-
-        while (n < randList.size()) {
-            if(sum < ws_size){
-                ws.add(rwset.get(randList.get(n)));
-                sum = sum + objs.get(randList.get(n)).getObj_size();
-            }
-            else{
-                rs.add(rwset.get(randList.get(n)));
-            }
-            n++;
-        }
-        if(rw == 0) {
-            return rs;
-        }
-        else{
-            return  ws;
-        }
-    }
-
-    private static List<Objects> getWriteSet(int write_rate, int rw_set_size, int total_objs){
-        List<Objects> ws = new ArrayList<Objects>();
-        int ws_size = rw_set_size * update_rate/800;
-        int sum = 0;
-        //Random rand = new Random();
-        while (sum<ws_size) {
-            Random rand = new Random();
-            int x = rand.nextInt(total_objs);
-            //if((sum + objs.get(x).getObj_size())<ws_size){
-                ws.add(objs.get(x));
-                sum = sum + objs.get(x).getObj_size();
-            //}
-        }
-        return ws;
-    }*/
 }
